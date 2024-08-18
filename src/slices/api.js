de6155a4b19f53cbbb994803d66671f557237e50
Sarmed Rizvi <sarmedrizvi@gmail.com>
2024-08-18 17:20:44 +0500

    api fix

--- src/slices/api.js
+++ src/slices/api.js
@@ -1,4 +1,4 @@
-export const url = "http://api.secondarydao.com/api";
+export const url = "https://api.secondarydao.com/api";
 // export const url = "http://localhost:5001/api";
 
 export const setHeaders = () => {
