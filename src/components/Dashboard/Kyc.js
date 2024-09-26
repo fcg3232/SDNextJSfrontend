fdf45ab687ff248f8c47f53b13b4ff1e88089be4
hamza <hamza@pop-os.localdomain>
2024-09-27 04:15:42 +0500

    kyc flow moved to verification_id & status check, address verification added

--- src/components/Dashboard/Kyc.js
+++ src/components/Dashboard/Kyc.js
@@ -1,279 +1,131 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { url, setHeaders } from "../../slices/api";
 import Button from "react-bootstrap/esm/Button";
 import Modal from "react-bootstrap/Modal";
 import { toast } from "react-toastify";
-// import { getUser } from "../../slices/authSlice";
-// import { getUser } from "../../slices/authSlice";
-// import styled from "styled-components";
-// import { usersFetchbyID } from "../../slices/UsersSlice";
-// import { useAppSelector, useAppDispatch } from "../../reducer/store";
-// import scrollreveal from "scrollreveal";
 
 const Kyc = () => {
-  // const dispatch = useAppDispatch();
-  // const navigate = useNavigate();
-  // const params = useParams();
-  const location = useLocation();
-  const dispatch = useDispatch();
-
-  // const [clientID, setclientID] = useState();
-  // const [Verification, setVerification] = useState();
-  // const [check, setcheck] = useState("");
-  // const [appID, setappID] = useState({});
-  // const [done, setdone] = useState(false);
   const [loading, setLoading] = useState(false);
   const [modalShow, setModalShow] = useState(false);
-  // const [showMenu, setShowMenu] = useState(false);
-  // const auth = useSelector((state) => state.auth);
-  // const id = useRef(0);
   const user = useSelector((state) => state.auth);
-  // const [checkTerm, setcheckTerm] = useState(false);
   const [users, setusers] = useState([]);
   const [userKycData, setUserKycData] = useState(null);
-  const [isKYCDataLoading, setIsKYCDataLoading] = useState(false);
-  const [applicantDataLoading, setApplicantDataLoading] = useState(false);
-  const [currentUser, setCurrentUser] = useState(null);
-  // const [productImg, setProductImg] = useState("");
-  const isRedirectedUser =
-    new URLSearchParams(location.search).get("redirected") === "true";
+  const [isKYCDataLoading, setIsKYCDataLoading] = useState(true);
+  const [userKycVerificationStatus, setUserKycVerificationStatus] =
+    useState(null);
 
   const headers = {
     "Content-Type": "application/json",
     Authorization: "Token e31169640d9147493929ab77c9128470b16d",
   };
-  console.log("user", user);
-
-  // useEffect(() => {
-  //   if (user._id) {
-  //     const fetchUserData = async () => {
-  //       // dispatch(getUser(user._id));
-  //       // try {
-  //       await axios
-  //         .get(`${url}/users/find/${user._id}`, {
-  //           headers: headers,
-  //         })
-  //         .then((currentUserResponse) => {
-  //           const { data } = currentUserResponse;
-  //           console.log("userRes.data", data);
-
-  //           console.log("data from user => ", data);
-  //           if (data._id && !isRedirectedUser && data.applicant_id) {
-  //             const fetchKYCData = async () => {
-  //               if (data._id) {
-  //                 try {
-  //                   setIsKYCDataLoading(true);
-  //                   setApplicantDataLoading(true);
-  //                   const kycRes = await axios.get(
-  //                     `${url}/kyc/find/${data._id}`,
-  //                     {
-  //                       headers: headers,
-  //                     }
-  //                   );
-  //                   setUserKycData(kycRes.data.kyc_data);
-  //                   setApplicantDataLoading(false);
-  //                   setIsKYCDataLoading(false);
-  //                 } catch (err) {
-  //                   setIsKYCDataLoading(false);
-  //                   setApplicantDataLoading(false);
-  //                   console.log("Error fetching KYC data:", err);
-  //                   toast.error("Error fetching KYC data");
-  //                 }
-  //               }
-  //             };
-
-  //             fetchKYCData();
-  //           }
-  //           if (data._id && isRedirectedUser && data.applicant_id) {
-  //             const MAX_RETRIES = 8;
-  //             let retries = 0;
-
-  //             const fetchKYCDataWithPolling = async () => {
-  //               try {
-  //                 setIsKYCDataLoading(true);
-  //                 setApplicantDataLoading(true);
-
-  //                 const kycRes = await axios.get(
-  //                   `${url}/kyc/find/${data._id}`,
-  //                   {
-  //                     headers: headers,
-  //                   }
-  //                 );
-
-  //                 setUserKycData(kycRes.data.kyc_data);
-  //                 setApplicantDataLoading(false);
-  //                 setIsKYCDataLoading(false);
-  //               } catch (err) {
-  //                 retries += 1;
-  //                 console.log(
-  //                   `Error fetching KYC data, attempt ${retries}:`,
-  //                   err
-  //                 );
-
-  //                 if (retries < MAX_RETRIES) {
-  //                   // Wait for a short delay before refetching
-  //                   setTimeout(fetchKYCDataWithPolling, 2000);
-  //                 } else {
-  //                   // Handle failure after max retries
-  //                   setIsKYCDataLoading(false);
-  //                   setApplicantDataLoading(false);
-  //                   console.log(
-  //                     "Failed to fetch KYC data after multiple attempts."
-  //                   );
-  //                   toast.error(
-  //                     "Failed to fetch KYC data after multiple attempts."
-  //                   );
-  //                 }
-  //               }
-  //             };
-
-  //             fetchKYCDataWithPolling();
-  //           }
-  //         });
-  //       // console.log("userResuserRes", userRes);
-  //       // } catch (err) {
-  //       //   toast.error("There was an error fetching user data");
-  //       // }
-  //     };
-  //     fetchUserData();
-  //   }
-  // }, [user._id]);
 
   useEffect(() => {
     if (user._id) {
       const fetchUserData = async () => {
         try {
-          // First API call to fetch user data
+          setIsKYCDataLoading(true);
           const currentUserResponse = await axios.get(
             `${url}/users/find/${user._id}`,
             {
               headers: headers,
             }
           );
-
           const { data } = currentUserResponse;
-          console.log("userRes.data", data);
-          console.log("data from user => ", data);
 
-          // KYC data fetching logic if applicable
-          if (data._id && !isRedirectedUser && data.applicant_id) {
+          if (data._id && data.applicant_id) {
             const fetchKYCData = async () => {
               if (data._id) {
                 try {
-                  setIsKYCDataLoading(true);
-                  setApplicantDataLoading(true);
                   const kycRes = await axios.get(
                     `${url}/kyc/find/${data._id}`,
                     {
                       headers: headers,
                     }
                   );
+                  // setting null to skip pending status ( see bottom )
+                  setUserKycVerificationStatus(null);
                   setUserKycData(kycRes.data.kyc_data);
-                  setApplicantDataLoading(false);
+                  console.log("kycRes.data", kycRes.data);
                   setIsKYCDataLoading(false);
+
+                  if (!kycRes.data.AMLAddressVerification) {
+                    const verifyAddress = async () => {
+                      // setLoading(true);
+                      try {
+                        const response = await axios.post(
+                          `${url}/kyc/verify-aml`,
+                          {
+                            walletAddress:
+                              "0x7e8Be455C9De1549B4B53c2b1fcF6c3F24972dc4",
+                            asset: "ETH",
+                          }
+                        );
+                        console.log("datadatadatadata", data);
+                        if (response.data) {
+                          await axios.patch(
+                            `${url}/kyc/${data._id}`,
+                            { AMLAddressVerification: response.data },
+                            setHeaders()
+                          );
+                        } else {
+                          toast.error("Error adding AML address verification");
+                          // handleError("Verification failed. Please try again.");
+                        }
+                      } catch (err) {
+                        console.error("Error verifying address:", err);
+                        toast.error("Error verifying address:", err);
+                      }
+                      // setLoading(false);
+                    };
+                    verifyAddress();
+                  }
                 } catch (err) {
                   setIsKYCDataLoading(false);
-                  setApplicantDataLoading(false);
                   console.log("Error fetching KYC data:", err);
                   toast.error("Error fetching KYC data");
                 }
               }
             };
             fetchKYCData();
-          }
-
-          // KYC data polling if applicable
-          if (data._id && isRedirectedUser) {
-            const MAX_RETRIES = 8;
-            let retries = 0;
-
-            const fetchKYCDataWithPolling = async () => {
+          } else if (data._id && data.verification_id) {
+            const getVerificationStatus = async () => {
               try {
-                setIsKYCDataLoading(true);
-                setApplicantDataLoading(true);
-
-                const kycRes = await axios.get(`${url}/kyc/find/${data._id}`, {
-                  headers: headers,
-                });
-
-                setUserKycData(kycRes.data.kyc_data);
-                setApplicantDataLoading(false);
-                setIsKYCDataLoading(false);
-              } catch (err) {
-                retries += 1;
-                console.log(
-                  `Error fetching KYC data, attempt ${retries}:`,
-                  err
+                const kycRes = await axios.get(
+                  `https://kyc-api.amlbot.com/verifications/${data.verification_id}`,
+                  {
+                    headers: headers,
+                  }
                 );
 
-                if (retries < MAX_RETRIES) {
-                  // Wait for a short delay before refetching
-                  setTimeout(fetchKYCDataWithPolling, 2000);
-                } else {
-                  // Handle failure after max retries
-                  setIsKYCDataLoading(false);
-                  setApplicantDataLoading(false);
-                  console.log(
-                    "Failed to fetch KYC data after multiple attempts."
-                  );
-                  toast.error(
-                    "Failed to fetch KYC data after multiple attempts."
-                  );
-                }
+                setUserKycVerificationStatus(kycRes.data);
+                setIsKYCDataLoading(false);
+              } catch (err) {
+                setIsKYCDataLoading(false);
+                console.log(`Error fetching KYC data, attempt`, err);
               }
             };
 
-            fetchKYCDataWithPolling();
+            getVerificationStatus();
           }
         } catch (err) {
           // Catch error for the first API call (user data) and show a toast
+          setIsKYCDataLoading(false);
           console.log("Error fetching user data:", err);
           toast.error("Error fetching user data");
         }
       };
-
       fetchUserData();
     }
   }, [user._id]);
-
-  // useEffect(() => {
-  //   if (user._id) {
-  //     dispatch(getUser({ id: user._id }));
-  //   }
-  // }, [user._id]);
-
-  const verifyAddress = async () => {
-    setLoading(true);
-    // setError('');
-    try {
-      // const response = await axios.post(`${url}/kyc/verify-aml`, {
-      //   walletAddress: "0x7e8Be455C9De1549B4B53c2b1fcF6c3F24972dc4",
-      //   asset: "ETH",
-      // });
-      const response = await axios.get(`${url}/kyc/proxy/coins`);
-      // const response = await axios.get(
-      //   "https://extrnlapiendpoint.silencatech.com/coins/",
-      //   {
-      //     Authorization: "Token e31169640d9147493929ab77c9128470b16d",
-      //     Accept: "application/json",
-      //   }
-      // );
-      console.log("response data from GET coins", response.data);
-    } catch (err) {
-      console.error("Error verifying address:", err);
-      toast.error("Error verifying address:", err);
-      // setError('Failed to verify address.');
-    }
-    setLoading(false);
-  };
+  // console.log("useruseruser", user);
 
   const formId = "8b32344e08c0454c312878540ce69ba5892c";
   const verify = () => {
     setLoading(true);
-    if (users != null) {
+    if (users != null && user._id) {
       axios
         .post(
           `https://kyc-api.amlbot.com/forms/${formId}/urls`,
@@ -285,10 +137,19 @@ const Kyc = () => {
             headers: headers,
           }
         )
-        .then((res) => {
+        .then(async (res) => {
           if (res.data.form_url) {
             setLoading(false);
-            window.location.href = res.data.form_url;
+            try {
+              await axios.patch(
+                `${url}/users/${user._id}`,
+                { verification_id: res.data.verification_id },
+                setHeaders()
+              );
+              window.location.href = res.data.form_url;
+            } catch (error) {
+              toast.error("Failed to update KYC verification ID:", error);
+            }
           }
         })
         .catch((error) => {
@@ -310,8 +171,22 @@ const Kyc = () => {
 
           console.error("Error during KYC verification", error);
         });
+    } else {
+      toast.error("Error starting KYC verification.");
     }
   };
+
+  if (userKycVerificationStatus !== null) {
+    return (
+      <div style={{ minHeight: "80vh" }}>
+        <div className="text ">
+          <h1 className="position-absolute top-50 start-50 translate-middle">
+            Verification Pending
+          </h1>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div style={{ minHeight: "80vh" }}>
@@ -373,11 +248,11 @@ const Kyc = () => {
         {/* ----- REMOVED A CONDITION FORM HERE FOR TESTING, CONTENT CAN BE SEEN BELOW */}
       </div>
 
-      <div className="mt-5">
+      {/* <div className="mt-5">
         <button onClick={verifyAddress} disabled={loading}>
           {loading ? "Verifying..." : "Verify Address"}
         </button>
-      </div>
+      </div> */}
 
       <Models show={modalShow} onHide={() => setModalShow(false)} />
 
